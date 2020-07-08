e049800553d604fbdf7b60a53b9777b9a60f2968
madHacks10000 <madeline.f.estey@gmail.com>
2020-07-08 00:51:34 +0000

    day two work all done

---
+++ intro_c/write_example/write_example.c
@@ -0,0 +1,28 @@
+// Include the uart header!
+#include "uart.h"
+
+int main(void)
+{
+  // Variable setup
+    char flag[50];
+    int i;
+    char str[]= "Beaver Works \n";
+    for (i = 0; i < 50; i++){
+        flag[i] = 0;
+    }
+  // Initialize serial
+    uart_init(UART2);
+  // Read characters from serial into a string until a newline is received
+    i = 0;
+    int ret;
+    uart_write_str(UART2, str);
+    flag[i] =  uart_read(UART2, BLOCKING, &ret);
+    while (flag[i] != '\n') {
+        i++;
+        flag[i]=uart_read(UART2, BLOCKING, &ret);
+    }
+  // Send the full string back over serial
+    uart_write_str(UART2, flag);
+    
+  return 0;
+}
